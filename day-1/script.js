dca3277fe93848aba75adb2c281cf6ced8290e1c
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2026-02-27 06:12:13 +0500

    initial

---
+++ day-1/script.js
@@ -0,0 +1,171 @@
+'use strict';
+
+// let userName = "Shokhjahon";
+// userName = "Abdulloh";
+// console.log(userName);
+
+// const userAge = 20;
+// console.log(userAge);
+
+// let ism = "Joe";
+// let familiya = "Doe"
+// console.log(ism);
+// console.log(familiya);
+
+// let mushuk = "Max";
+// let egaFamiliya = "Doe";
+// console.log(mushuk);
+// console.log(egaFamiliya);
+
+// const city = "Toshkent";
+// let aholisi = 1500000;
+// aholisi = 1550000;
+// console.log(`city: ${city}`);
+// console.log(`aholisi: ${aholisi}`);
+
+// let son = 10;
+// son = son +5;
+// console.log(son);
+
+// let a = 8;
+// let b = 3;
+// console.log(a + b);
+
+// const pi = 3.14;
+// pi = 3.17;
+// console.log(pi);
+
+// let yosh = 23;
+// yosh = 24;
+// const ism = 'Joe';
+// const tugilganYil = 2001;
+// let kasb = "Frontend dasturchi";
+// kasb = "Fullstack dasturchi";
+// let davlat ="O'zbekiston";
+// davlat = "AQSH";
+// console.log(`Mening yoshim: ${yosh}`);
+// console.log(`Mening ismim: ${ism}`);
+// console.log(`Tug'ilgan yilim: ${tugilganYil}`);
+// console.log(`Meing kasbim: ${kasb}`);
+// console.log(`Men hozir ${davlat} da yashayabman.`);
+
+
+// let olma = 3.5;
+// let banan = 2.0;
+// let uzum = 5.0;
+// console.log(olma + banan + uzum);
+
+// let haqiqat = true;
+// let yolgon = false;
+// console.log(haqiqat);
+
+// let quyosh = false;
+// let yomgir = true;
+// let qor = true;
+// console.log(`Quyosh nuri mavjudmi: ${quyosh}`);
+// console.log(`Yomgir yog'adimi: ${yomgir}`);
+// console.log(`Qor yog'adimi: ${qor}`);
+
+// let internet = true;
+// let uzilish = false;
+// let tezlik = true;
+// console.log(`Internet mavjudmi: ${internet}`);
+// console.log(`Xizmatda uzilish bormi: ${uzilish}`);
+// console.log(`Tezlik yuqorimi: ${tezlik}`);
+
+// let ism;
+// console.log(ism);
+
+// let qiymat = null;
+// console.log(qiymat);
+
+// let ism;
+// let familiya = null;
+// console.log(ism);
+// console.log(familiya);
+
+// let person = {
+//     ism: 'Shokhjahon',
+//     yosh: 18,
+//     kasb: "Dasturchi",
+// }
+// console.log(person.kasb);
+
+// let car = {
+//     model: "Gentra",
+//     color: "Black",
+//     isNew: true
+// }
+// console.log(car.isNew);
+
+// let colors = ["green", "black", "white"];
+// console.log(colors[0]);
+
+// let mevalar = ["olma", "banan", "apelsin"]
+// console.log(mevalar[1]);
+
+// alert("Saytimizga xush kelibsiz");
+// const xabar = "Saytimizga xush kelibsiz";
+// alert(xabar);
+
+// let vaqt = "13:30";
+// alert(`Bugungi uchrashuv vaqti: ${vaqt}`);
+
+// let yosh = confirm("Yoshingiz 18 dan balandmi?");
+// alert(yosh);
+
+// let user = confirm("Bu sayt sizga yoqdimi?")
+// alert(user);
+
+// let yosh = +prompt("Yoshingizni kiriting");
+// alert(yosh);
+
+// console.log("Shokhjahon " + "Qalandarov");
+
+// let ism = prompt("Ismingizni kirting: ");
+// let familiya = prompt("Familyangizni kiriting: ");
+// alert(ism + familiya);
+
+// let ism = prompt("Ismingizni kiriting: ");
+// let yosh = +prompt("Yoshingizni kiriting: ");
+// alert(`Assalomu Alekum ${ism} Siz ${yosh} yoshdasiz.`)
+
+// let asistent1 = prompt("Hurmatli asistent ismingizni kiriting: ");
+// let asistent2 = prompt("Hurmatli asistent ismingizni kiriting: ");
+// let asistent3 = prompt("Hurmatli asistent ismingizni kiriting: ");
+// alert(`Bu assistentlar sizning jamoangizdagi a'zolar: ${asistent1}, ${asistent2}, ${asistent3}.`);
+
+// let ytKanal = prompt("Sevimli youtube kanalingizni yozing: ")
+// alert(`https://youtube.com/@${ytKanal}`);
+
+console.log(8 + 7);
+console.log(17 - 11);
+console.log(8 * 7);
+console.log(56 / 8);
+console.log(10 % 3);
+console.log(2 ** 3);
+
+// let son1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+// alert(`${son1} + ${son2} = ${son1 + son2}`);
+
+// let a = +prompt("Son kiriting")
+// let b = +prompt("Son kiriting")
+// let sum = a + b;
+// alert(`${a} + ${b} = ${sum}`)
+
+// let numberOfSeries = +prompt("Nechta serial ko'rdingiz? ");
+// // alert(`Siz ${numberOfSeries}ta serial ko'rgansiz.`)
+
+// let seriesDB = {
+//     count: numberOfSeries,
+//     series: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// }
+// console.log(seriesDB);
+
+console.log(7 == '7');
+console.log(7 === '7');
+
