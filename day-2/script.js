f7dda15469e99146644488a90dbea2778404ce64
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2026-03-04 12:07:06 +0500

    initial

---
+++ day-2/script.js
@@ -0,0 +1,43 @@
+// FUNKSIYA
+
+// Function declaration
+// function kvadrat(son) {
+//     return son * son;
+// }
+// console.log(kvadrat(10));
+
+// Function expression
+// let kvadrat = function(son) {
+//     return son * son;
+// }
+// console.log(kvadrat(7)); // 49
+
+
+// Arrow function
+// let hello = () => {
+//     console.log("Hello");
+// }
+// hello();
+
+
+// let ism = prompt("Ismingizni kiriting: ");
+// let salom = (name) => {
+//     alert(`Salom ${name}`);
+// }
+// salom(ism);
+
+// let yigindi = (a, b) => {
+//     console.log(a + b);
+// }
+// yigindi(7, 3);
+
+// let sum = (a, b) => {
+//     return a + b
+// }
+// console.log(sum(3, 7));
+
+// let calc = (a, b) => a + b;
+// console.log(calc(7, 7));
+
+
+// STRING METHODLAR
